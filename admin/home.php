f081690dad185c6a0b1fd7a20cc31affc31d7403
oraldo22 <oraldo2010@gmail.com>
2016-05-30 02:34:53 +0200

    update Admin

--- admin/home.php
+++ admin/home.php
@@ -20,11 +20,10 @@ if(!isset($_SESSION["sess_user"])){
 			margin-bottom: 20px;
 		}
 		#liste_doktoresh{
-			margin-top: 100px;
+			margin-top: 170px;
 			border: 2px solid #ccc;
 			width: 500px;
 			height: 500px;
-	
 			display: inline-block;
 
 		}
@@ -34,18 +33,60 @@ if(!isset($_SESSION["sess_user"])){
 			border: 2px solid #ccc;
 			width: 500px;
 			height: 500px;
-			display: inline-block;
+			display: block;
 		}
 
+		.logout_button{
+			    float: right;
+   				margin-top: 20px;
+    			position: relative;
+    			z-index: 1002;
+    			height: 40px;
+    			width: 100px;
+    			background-color: #FF9933;
+		}
+
+		body {
+			height: 850px;
+   			border: none;
+    		background-image: url("test1.png");
+   			background-color: #E6E8D8;
+    		opacity: 0.9;
+		}
+
+		#logo {
+    		background-image: url("logoemedica2.png");
+    		background-repeat: no-repeat;
+    		background-size: 70%;
+    		position: absolute;
+    		width: 40%;
+    		height: 30%;
+    		margin-left: 3%;
+    		z-index: 10002;
+    		margin-top: 10px;
+		}
+
+
+		#header_image {
+    background: url("header.png");
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    width: 1280px;
+    height: 80px;
+}
 		</style>
 	</head>
 	<body>
 		<div>
-			<a href="../logout.php"><button style="cursor:pointer" class="logout_button">Log Out</button></a>
+			<a href="../logout.php"><button class="logout_button">Log Out</button></a>
 		</div>
 			<div>
+			<div>
+			<div id="header_image"></div>
+			<div id="logo"></div>
 				<div class="panel">
 						<div class="panel_content">
+
 							<div id="liste_doktoresh">	
 								<h2 style="text-align:center">Lista e doktoreve</h2>
 							
@@ -79,6 +120,7 @@ if(!isset($_SESSION["sess_user"])){
 						
 				</div>
 			</div>	
+			</div>
 	</body>
 </html>
 <?php
