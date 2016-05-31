0219f9be8afc72986625aa99fdccfab8bfd89faf
oraldo22 <oraldo2010@gmail.com>
2016-05-31 02:36:12 +0200

    update2adm

--- admin/home.php
+++ admin/home.php
@@ -21,19 +21,19 @@ if(!isset($_SESSION["sess_user"])){
 		}
 		#liste_doktoresh{
 			margin-top: 170px;
-			border: 2px solid #ccc;
 			width: 500px;
 			height: 500px;
-			display: inline-block;
+			background: url("shiritatmenu2.png");
+    		background-repeat: no-repeat;
 
 		}
 
 		#liste_pacientesh{
 			margin-top: 100px;
-			border: 2px solid #ccc;
 			width: 500px;
 			height: 500px;
-			display: block;
+			background: url("shiritatmenu2.png");
+    		background-repeat: no-repeat;
 		}
 
 		.logout_button{
@@ -66,13 +66,30 @@ if(!isset($_SESSION["sess_user"])){
     		z-index: 10002;
     		margin-top: 10px;
 		}
-
-
+		.panel_content {
+			width: 500px !important;
+		}
+		#divi{
+			display: inline-block;
+		}
+		#divi2{
+			display: inline-block;
+			padding-left: 120px;
+		}
+		#id1{
+			    margin-left: 120px;
+    padding-top: 16px;
+    font-size: 24px;
+    margin-top: 10px;
+    font-family: "Comic Sans MS", "Comic Sans";
+    font-weight: bold;
+    color: #F9A825;
+		}
 		#header_image {
     background: url("header.png");
     background-repeat: no-repeat;
     background-size: 100% 100%;
-    width: 1280px;
+    width: 100%;
     height: 80px;
 }
 		</style>
@@ -81,14 +98,15 @@ if(!isset($_SESSION["sess_user"])){
 		<div>
 			<a href="../logout.php"><button class="logout_button">Log Out</button></a>
 		</div>
-			<div>
-			<div id="header_image"></div>
+		<div id="header_image"></div>
 			<div id="logo"></div>
+			<div id="divi">
+			
 				<div class="panel">
 						<div class="panel_content">
 
 							<div id="liste_doktoresh">	
-								<h2 style="text-align:center">Lista e doktoreve</h2>
+								<h2 id="id1" style="text-align:center; background-image:url("shiritmenu2.png")">Lista e doktoreve</h2>
 							
 								<?php include 'getListeDoktoresh.php'; ?>
 		
@@ -103,11 +121,11 @@ if(!isset($_SESSION["sess_user"])){
 			</div>	
 
 			<!--Liste pacientesh -->
-			<div>
+			<div id="divi2">
 				<div class="panel">
 						<div class="panel_content">
 							<div id="liste_pacientesh">	
-								<h2 style="text-align:center">Lista e pacienteve</h2>
+								<h2 id="id1" style="text-align:center">Lista e pacienteve</h2>
 							
 								<?php include 'getListePacientesh.php'; ?>
 		
